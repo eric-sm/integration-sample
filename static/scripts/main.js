2e517d9aa9f95239eff249e449b2507dde9f02d4
Eric Small <eric@jericsmall.com>
2020-02-26 15:44:13 -0500

    Set up index and remove environment validation code from main.js and HEAD

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,26 +1,2 @@
 'use strict';
 
-const e = React.createElement;
-
-class MainButton extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { clicked: false };
-    }
-
-    render() {
-        if (this.state.clicked) {
-            return 'You clicked the button. Good job!';
-        }
-
-        return e('button',
-            { onClick: () => this.setState({ clicked: true }) },
-            'ClickMe!'
-        );
-    }
-}
-
-function onLoad() {
-    const domContainer = document.querySelector('#main_container');
-    ReactDOM.render(e(MainButton), domContainer);
-}
