5aecdb4526dabd67285131626203dd97826c29e4
Eric Small <eric@jericsmall.com>
2020-02-26 17:09:32 -0500

    Added dynamic JSX support using Babel

---
+++ static/scripts/environmentValidation.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const e = React.createElement;
+
+class MainButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { clicked: false };
+    }
+
+    render() {
+        if (this.state.clicked) {
+            return 'You clicked the JSX button. Good job!';
+        }
+
+        return (
+            <button onClick={() => this.setState({ clicked: true })}>
+                ClickMe!
+            </button>
+        );
+    }
+}
+
+const domContainer = document.querySelector('#main_container');
+ReactDOM.render(e(MainButton), domContainer);
