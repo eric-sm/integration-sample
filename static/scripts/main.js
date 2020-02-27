c4c6079caa4a91f915cbd33b8905b3a600bcdbf5
Eric Small <eric@jericsmall.com>
2020-02-27 02:18:15 -0500

    Display results with hate group flagged in red; link to profile except flagged charities

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -49,3 +49,58 @@ sampleCharitySearchResponse.result.forEach( charity => {
         }
     });
 });
+
+
+// Class definition of a charity rendering for a charity from the search results
+class Charity extends React.Component {
+    constructor(props) {
+        super(props);
+        this.gotoProfile = this.gotoProfile.bind(this);
+    }
+
+    // Redirect the browser to the Givz.com profile page for the charity
+    // Sample profile page URL: https://givz.com/charity/globe-aware-030507729
+    gotoProfile() {
+         let nameFormatted = this.props.charity.name.toLowerCase().replace(/\s/g, "-");
+         let ein = this.props.charity.ein;
+
+         window.location = "https://givz.com/charity/" + nameFormatted + "-" + ein;
+     }
+
+    render() {
+        return (
+            <li className={'charity ' + (this.props.charity.is_hate_group ? "hate-group alert alert-danger" : "")} onClick={this.gotoProfile}>
+                <div className='name'>{this.props.charity.name}</div>
+                <div className='city'>{this.props.charity.city}</div>
+                <div className='state'>{this.props.charity.state}</div>
+                <div className='ein'>{this.props.charity.ein}</div>
+                <div className='ein'>{this.props.charity.is_hate_group}</div>
+                <div className='donate-button'>Donate</div>
+            </li>
+        );
+    }
+}
+
+
+// Class definition of search results for a charity search
+class CharitySearchResults extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { results: props.results };
+    }
+
+    render() {
+        return (
+            <ol className='search-results'>
+                {this.state.results.map((result) => <Charity key={result.ein} charity={result} /> )}
+            </ol>
+        )
+    }
+}
+
+
+
+// Display the results in the page
+const resultsElement = <CharitySearchResults results={sampleCharitySearchResponse.result} />;
+const domContainer = document.querySelector('#search_results');
+ReactDOM.render(resultsElement, domContainer);
